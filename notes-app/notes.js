ff03225511d281b71a7864fbd074f35c3ea4cc07
Julien Rios <julienrios1@gmail.com>
2020-06-02 12:49:10 -0400

    yargs commands/debugger

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -6,6 +6,8 @@ const addNote = function(title, body){
     const duplicateNotes = notes.filter((note) => note.title === title)
     const duplicateNote = notes.find((note)=> note.title === title )
 
+    debugger
+
     if(!duplicateNote){
         notes.push({
         title: title,
