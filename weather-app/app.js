29bc143ccac0762efef992849f7b5f8ca2358d71
Julien Rios <julienrios1@gmail.com>
2020-06-19 12:51:56 -0400

    CLI for forecasting

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,22 @@
-const request = require('request')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-// const url = 'http://api.weatherstack.com/current?access_key=cd2d57c0c33155d142b6e6d6c8bd70bb&query=37.8267,-122.4233'
+if(process.argv[2]){
+  geocode(process.argv[2], (error, data)=>{
+    if(error){
+      return console.log(error)
+    }
+      
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if(error){
+      return console.log(error)
+    }
+    console.log(data.location)
+    console.log(forecastData)
+    })
+  })
+}else{
+  console.log("Please provide a location using command line")
+}
 
-// request({url: url, json: true}, (error, response) => {
-//     if(error){
-//         console.log('Unable to connect to weather service')
-//     }else if(response.body.error){
-//         console.log('Unable to find location')
-//     } else{
-//         console.log(response.body.current.weather_descriptions[0]+`. It is currently ${response.body.current.temperature} degrees out and it feels like ${response.body.current.feelslike} degrees out.`)
-//     }
-// }) 
 
-// geocode('Boston', (error, data)=>{
-//     console.log('Error', error)
-//     console.log('Data', data)
-// })
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(37.8267,-122.4233, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
